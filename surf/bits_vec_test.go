c694222db0a9acb12c590cf6a474b4c783938cb2
Zejun Li <me@bobotu.dev>
2019-12-02 19:06:33 +0800

    add label vector of SuRF (#138)

--- surf/bits_vec_test.go
+++ surf/bits_vec_test.go
@@ -186,3 +186,23 @@ func BenchmarkSelect64(b *testing.B) {
 		}
 	})
 }
+
+func TestLabelVecSearch(t *testing.T) {
+	labels := [][]byte{
+		{1},
+		{2, 3},
+		{4, 5, 6},
+		{labelTerminator, 7, 8, 9},
+	}
+	v := new(labelVector)
+	v.Init(labels, 0, uint32(len(labels)))
+	labelShouldExist := func(k byte, start, size, pos uint32) {
+		r, ok := v.Search(k, start, size)
+		require.True(t, ok)
+		require.Equal(t, pos, r)
+	}
+	labelShouldExist(1, 0, 1, 0)
+	labelShouldExist(3, 0, 5, 2)
+	labelShouldExist(5, 3, 7, 4)
+	labelShouldExist(7, 6, 8, 7)
+}
